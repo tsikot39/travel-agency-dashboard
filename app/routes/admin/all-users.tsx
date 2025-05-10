6f066dd9928e2c8c0121cad5aa53bd96643c261e
tsikot39 <corpusjohnbenedict@gmail.com>
2025-05-10 12:14:43 -0600

    first commit

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,16 @@
+import { Header } from "components";
 import React from "react";
 
 const AllUsers = () => {
-  return <div>Users Table</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users in real time"
+      />
+      All Users Page Contents
+    </main>
+  );
 };
 
 export default AllUsers;
